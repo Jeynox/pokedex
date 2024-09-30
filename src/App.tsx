9439a20d0d0907c1f2ecd00b99ca1cd51766b145
Daniel Lemos <daniel.lemos.jose1998@gmail.com>
2024-10-01 01:06:05 +0200

    ajout des props

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,23 @@ import { PokemonCard } from './components/PokemonCard';
 
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
   return (
     <section>
       <div>
-        <PokemonCard />
+        <PokemonCard 
+          name={pokemonList[0].name}
+          img={pokemonList[0].imgSrc}
+        />
       </div>
     </section>
   );
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,20 +1,10 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-export function PokemonCard() {
-    const pokemon = pokemonList[1];
+
+export function PokemonCard({img, name}) {
     return (
         <figure>
-            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>}
-            <figcaption>{pokemon.name}</figcaption>
+            {img ? <img src={img} alt={name}/> : <p>???</p>}
+            <figcaption>{name}</figcaption>
         </figure> 
     )
     
